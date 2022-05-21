fa832048a9a8d842365e7225415f4d895a80c002
buzhifanji <buzhifanji@163.com>
2022-05-21 13:22:10 +0800

    修改 docs 文件命名

--- packages/playground/src/router.ts
+++ packages/playground/src/router.ts
@@ -1,10 +1,10 @@
 import kebabcase from "lodash.kebabcase";
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
-const viewModules = import.meta.glob("../../docs/components/**/*.vue");
+const viewModules = import.meta.glob("../../docs/demos/**/*.vue");
 
 // const nameFromPath = (path: string) => path.replace(/^.*\/(\w+)\.vue$/, "$1");
-const nameDir = (path: string) => path.match(/(components\/=?)(\S*)/);
+const nameDir = (path: string) => path.match(/(demos\/=?)(\S*)/);
 
 const pages: RouteRecordRaw[] = Object.keys(viewModules).map((path) => {
   const name = nameDir(path)![2].replace(/.vue/, "");
