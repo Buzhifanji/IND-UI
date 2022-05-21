689ac4499e815dd835b2186c709ed2041b9fffbb
buzhifanji <buzhifanji@163.com>
2022-05-21 18:01:58 +0800

    spin 添加测试

--- packages/spin/src/spin.test.ts
+++ packages/spin/src/spin.test.ts
@@ -3,9 +3,16 @@ import { html } from "lit";
 import { type ISpin } from "./spin";
 
 describe("ISpin", () => {
-  it("default values are set correctly", async () => {
-    const el = await fixture<ISpin>(html`<i-spin>default</i-spin>`);
+  it("should pass accessibility tests", async () => {
+    const el = await fixture<ISpin>(html`<i-spin></i-spin>`);
 
-    expect(el.example).to.equal("");
+    await expect(el).to.be.accessible();
+  });
+
+  it("should have a role fo status", async () => {
+    const el = await fixture<ISpin>(html`<i-spin></i-spin>`);
+
+    const base = el.shadowRoot!.querySelector('[part="base"]')!;
+    expect(base).have.attribute("role", "status");
   });
 });
